30483b79645b8c39ee982f49a435946eca5e3ca0
daye-han <handot38@gmail.com>
2021-01-25 22:14:20 +0900

    showScrollTopButton()함수 javascript로 작성

--- js/common.ui.js
+++ js/common.ui.js
@@ -17,27 +17,24 @@ function scrollAnimate(value, time) {
     $('html,body').animate({ scrollTop: value }, time);
 }
 
-// scroll top button fixed
-scrollTopButtonFixFn();
-function scrollTopButtonFixFn() {
-    var $topBtn = $('.scroll-top');
-    if(!$topBtn.length) return;
+// scroll top button show/hide
+showScrollTopButton();
+function showScrollTopButton() {
+    var topBtn = document.querySelector('.scroll-top');
+    var current;
+    if(!topBtn) return;
 
-    var _winHeight = $(window).height();
-    var _current;
-
-    $(window).on('scroll', function () {
-        _current = $(window).scrollTop();
-        if(_current > _winHeight) {
-            $topBtn.addClass('is-fixed');
+    document.addEventListener('scroll', function () {
+        current = window.pageYOffset;
+        if (current > window.innerHeight){
+            topBtn.classList.add('is-fixed');
         } else {
-            $topBtn.removeClass('is-fixed');
+            topBtn.classList.remove('is-fixed');
         }
-    })
+    });
 }
 
 // scroll opacity animaiton
-// gsap.registerPlugin(ScrollTrigger);
 var sections = document.querySelectorAll('.scroll-ani');
 for(var i=0; i<sections.length; i++){
     var section = sections[i];
@@ -47,70 +44,6 @@ for(var i=0; i<sections.length; i++){
             trigger: section,
             start: 'top bottom-=250',
             toggleActions: 'play none none reverse',
-            // markers: { startColor: "green", endColor: "red", fontSize: "12px" },
         }
     });
-    // ScrollTrigger.create({
-    //     trigger: section,
-    //     start: 'top center',
-    //     end: () => `+=${section.clientHeight + 30}`,
-    //     toggleActions: 'play reverse none reverse',
-    // })
 }
-
-// sections.forEach(function (section, index) {
-//     gsap.to(section, {
-//         autoAlpha: 1,
-//         scrollTrigger: {
-//             trigger: section,
-//             start: 'top bottom-=300',
-//             toggleActions: 'play none none reverse',
-//             // markers: true
-//         }
-//     });
-
-//     ScrollTrigger.create({
-//         trigger: section,
-//         id: index + 1,
-//         start: 'top center',
-//         // end: () => `+=${section.clientHeight + 30}`,
-//         toggleActions: 'play reverse none reverse',
-//         // toggleClass: { targets: section, className: "is-active" },
-//         // markers: true
-//     })
-// })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// sections.forEach((section, index) => {
-//     gsap.to(section, {
-//         autoAlpha: 1,
-//         scrollTrigger: {
-//             trigger: section,
-//             start: 'top bottom-=300',
-//             toggleActions: 'play none none reverse',
-//             // markers: true
-//         }
-//     });
-
-//     ScrollTrigger.create({
-//         trigger: section,
-//         id: index + 1,
-//         start: 'top center',
-//         end: () => `+=${section.clientHeight + 30}`,
-//         toggleActions: 'play reverse none reverse',
-//         toggleClass: { targets: section, className: "is-active" },
-//         // markers: true
-//     })
-// })
